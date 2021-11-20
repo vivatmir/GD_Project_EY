a36f3b5b0f8078ea5c45874ac0c68025a015caf2
Katerina <solo-disk@mail.ru>
2021-11-20 18:59:48 +0300

    file

---
+++ Часть 1/user2018.py
@@ -0,0 +1,18 @@
+import pandas as pd
+df = pd.read_csv('gamedev38/events.csv')
+df2 = pd.read_csv('gamedev38/purchase.csv')
+cond = (df.start_time>='2018-01-01') & (df.start_time<'2019-01-01') & (df.event_type=='registration')
+cond2 = (df2.event_datetime>='2018-01-01') & (df2.event_datetime<'2019-01-01') & (df.event_type=='registration')
+registered = df[cond]['user_id'].to_list()
+registered2 = df2[cond2]['user_id'].to_list()
+events = df[df.user_id.isin(registered)]
+events.start_time = pd.to_datetime(events.start_time, format='%Y-%m-%dT%H:%M:%S')
+df2['event_type'] = 'purchase'
+purchase = df2[df2.user_id.isin(registered2)]
+purchase.event_datetime = pd.to_datetime(purchase.event_datetime, format='%Y-%m-%dT%H:%M:%S')
+print(events.start_time)
+print(purchase.event_datetime)
+
+
+
+
